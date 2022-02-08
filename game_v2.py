95e4410ce67cb4ea2a3a23a2c930d90196a83ea1
Vladislav-Pashkov <vladislav-pavlovich@yandex.ru>
2022-02-08 21:11:59 +0700

    Initial

--- game_v2.py
+++ game_v2.py
@@ -46,4 +46,4 @@ def score_game(random_predict) -> int:
 if __name__ == '__main__':
     score_game(random_predict)
     
-print(f'Количество попыток: {random_predict()}')
+#print(f'Количество попыток: {random_predict()}')
